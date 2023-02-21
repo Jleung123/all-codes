a5ccbec12a692e204207cdd2a07acfae548fc74d
Jleung123 <57479120+Jleung123@users.noreply.github.com>
2023-02-21 01:31:57 -0500

    Add files via upload

---
+++ CIS 485/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Samsung White</title>
+		<link href = "https://fonts.googleapis.com/css?family = Quicksand:300,400,500,700,900" rel = "stylesheet">
+		<link rel = "stylesheet" href = "view/fonts/icomoon/style.css">
+		<link rel = "stylesheet" href = "view/css/bootstrap.min.css">
+		<link rel = "stylesheet" href = "view/css/style.css">
+	</head>
+	<body data-spy = "scroll" data-target = ".site-navbar-target" data-offset = "300">
+		<!-- top link to other pages-->
+		<div class = "site-wrap">
+			<div class = "site-mobile-menu site-navbar-target">
+				<div class = "site-mobile-menu-header">
+					<div class = "site-mobile-menu-close mt-3">
+						<span class = "icon-close2 js-menu-toggle"></span>
+					</div>
+				</div>
+				<div class = "site-mobile-menu-body"></div>
+			</div>   
+			<div class = "border-bottom top-bar py-2 bg-dark" id = "home-section">
+				<div class = "container">
+					<div class = "row"></div>
+				</div> 
+			</div>
+			<!-- end of top link to other pages-->
+			<!-- header for munu -->
+			<header class = "site-navbar py-4 bg-white js-sticky-header site-navbar-target" role = "banner">
+				<div class = "container">
+					<div class = "row align-items-center">
+						<div class = "col-11 col-xl-2">
+							<h1 class = "mb-0 site-logo"><a href = "index.php" class = "text-black h2 mb-0"><span class = "text-primary">Samsung White</span> </a></h1>
+						</div>
+						<div class = "col-12 col-md-10 d-none d-xl-block">
+							<nav class = "site-navigation position-relative text-right" role = "navigation">
+								<ul class = "site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+									<li><a href = "index.php" class = "nav-link">Home</a></li>
+									<li><a href = "view/about.php" class = "nav-link">About Us</a></li>
+									<li><a href = "view/contact.php" class = "nav-link">Contact Us</a></li>
+									<li><a href = "view/products.php" class = "nav-link">Products</a></li>
+								</ul>
+							</nav>
+						</div>
+						<div class = "d-inline-block d-xl-none ml-md-0 mr-auto py-3" style = "position: relative; top: 3px;"><a href = "#" class = "site-menu-toggle js-menu-toggle text-black"><span class = "icon-menu h3"></span></a></div>
+					</div>
+				</div>
+			</header>
+			<!-- end of header for menu-->
+			<!-- intro artwork for index -->
+			<div class = "site-blocks-cover overlay" style = "background-image: url(view/images/samsung_intro.jpg);" data-aos = "fade" data-stellar-background-ratio = "0.5">
+				<div class = "container">
+					<div class = "row align-items-center justify-content-center text-center">
+						<div class = "col-md-12" data-aos = "fade-up" data-aos-delay = "400">    
+							<div class = "row justify-content-center mb-4">
+								<div class = "col-md-8 text-center">
+									<h1>The one stop shop for all your appliance needs<span class = "typed-words"></span></h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- end of intro artwork for index -->	
+			<!-- display message for index page -->
+			<section class = "site-section">
+				<div class = "container">
+					<h2 class = "text-primary">Welcome to Samsung White</h2>
+					<p class = "mb-4">We here at Samsung White undertand that buying appliances from our
+						competitors can be a hassle. Which is why we provide unlimited warranty on all our products</p>
+					<p class = "mb-4">We also undertand that some customers are having a hard time decide which
+						products to use. Which is why we have spent the past 10 years researching and came up with 
+						an increasing line of products that seamlessly works and integrates with our other products.</p>
+					<p class = "mb-4">As we say here at Samsung White "We aren't satisified until you are satisified"</p>
+				</div>
+			</section>
+			<!-- end of display message for index page --> 
+			<footer class = "site-footer">
+				<div class = "container">
+					<div class = "row">
+						<div class = "col-md-3">
+							<h2 class = "footer-heading mb-4">Subscribe Newsletter</h2>
+							<form action = "#" method = "post">
+								<div class = "input-group mb-3">
+									<input type = "text" class = "form-control border-secondary text-white bg-transparent" placeholder = "Enter Email" aria-label = "Enter Email" aria-describedby = "button-addon2">
+									<div class = "input-group-append">
+										<button class = "btn btn-primary text-white" type = "button" id = "button-addon2">Send</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class = "row pt-5 mt-5 text-center">
+						<div class = "col-md-12">
+							<div class = "border-top pt-5">
+								<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+									Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class = "icon-heart text-danger" aria-hidden = "true"></i> by <a href = "https://colorlib.com" target = "_blank" >Colorlib</a>
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+		<!-- site-wrap -->
+		<script src = "view/js/jquery-3.3.1.min.js"></script>
+		<script src = "view/js/jquery-migrate-3.0.1.min.js"></script>
+		<script src = "view/js/owl.carousel.min.js"></script>
+		<script src = "view/js/jquery.stellar.min.js"></script>
+		<script src = "view/js/jquery.easing.1.3.js"></script>
+		<script src = "view/js/aos.js"></script>
+		<script src = "view/js/main.js"></script>
+		<!-- end of site-wrap -->
+	</body>
+</html>
